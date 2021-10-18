701de676c799f0bb1a054aae6e042ff974333fb0
victuelles <rom.victuelles@gmail.com>
2021-10-18 16:08:41 -0700

    container code added

---
+++ container/src/bootstrap.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+import App from './App'
+
+//mount function to stat up the app
+ 
+    ReactDOM.render(
+        <App/>,document.querySelector("#root")
+    )
+ 
+ 
